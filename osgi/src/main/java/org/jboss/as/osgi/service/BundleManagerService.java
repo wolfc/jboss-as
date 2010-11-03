f26f4065067303701d31f2b9ea59733d5aa38ad4
Thomas Diesler <thomas.diesler@jboss.com>
2010-11-03 16:19:40 +0100

    [JBOSGI-420] Unexpected null wiring for JAXBContext

--- osgi/src/main/java/org/jboss/as/osgi/service/BundleManagerService.java
+++ osgi/src/main/java/org/jboss/as/osgi/service/BundleManagerService.java
@@ -56,6 +56,7 @@ import org.jboss.osgi.framework.Constants;
 import org.jboss.osgi.framework.bundle.BundleManager;
 import org.jboss.osgi.framework.bundle.BundleManager.IntegrationMode;
 import org.jboss.osgi.framework.plugin.ModuleManagerPlugin;
+import org.jboss.osgi.framework.plugin.SystemPackagesPlugin;
 
 /**
  * Service responsible for creating and managing the life-cycle of the OSGi {@link BundleManager}.
@@ -94,31 +95,38 @@ public class BundleManagerService implements Service<BundleManager> {
             // Setup the OSGi {@link Framework} properties
             Configuration config = injectedConfig.getValue();
             Map<String, Object> props = new HashMap<String, Object>(config.getProperties());
-            ServiceContainer container = context.getController().getServiceContainer();
-            ModuleLoader moduleLoader = injectedModuleLoader.getValue().getModuleLoader();
-            Module frameworkModule = new FrameworkModuleLoader(moduleLoader, props).getFrameworkModule();
-            props.put(IntegrationMode.class.getName(), IntegrationMode.CONTAINER);
-            props.put(ModuleLoader.class.getName(), moduleLoader);
-            props.put(ServiceContainer.class.getName(), container);
-            props.put(Module.class.getName(), frameworkModule);
 
-            // [TODO] This is a temporary workaround to configure the HTTP subsystem in OSGi
-            // this will go away once the HTTP subsystem from AS implements the OSGi HTTP Service.
+            // Set the Framework's {@link IntegrationMode}
+            props.put(IntegrationMode.class.getName(), IntegrationMode.CONTAINER);
+
+            // Setup the default {@link ModuleLoader}
+            ModuleLoader moduleLoader = injectedModuleLoader.getValue().getModuleLoader();
+            props.put(ModuleLoader.class.getName(), moduleLoader);
+
+            // Setup the {@link ServiceContainer}
+            ServiceContainer container = context.getController().getServiceContainer();
+            props.put(ServiceContainer.class.getName(), container);
+
+            // Configure the OSGi HttpService port
+            // [TODO] This will go away once the HTTP subsystem from AS implements the OSGi HttpService.
             props.put("org.osgi.service.http.port", "" + osgiHttpServerPortBinding.getValue().getSocketAddress().getPort());
 
-            // Always clean the framework storage
-            // [TODO] Differentiate beetween user data and persisted bundles
+            // Setup the Framework's storage area. Always clean the framework storage on first init.
+            // [TODO] Differentiate beetween user data and persisted bundles. Persist bundle state in the domain model.
             props.put(Constants.FRAMEWORK_STORAGE_CLEAN, Constants.FRAMEWORK_STORAGE_CLEAN_ONFIRSTINIT);
 
             // Get {@link ModuleLoader} for the OSGi layer
             bundleManager = new BundleManager(props);
-            ModuleManagerPlugin plugin = bundleManager.getPlugin(ModuleManagerPlugin.class);
-            ModuleLoader osgiModuleLoader = plugin.getModuleLoader();
+
+            // Setup the Framework {@link Module}
+            Module frameworkModule = new FrameworkModuleLoader(bundleManager).getFrameworkModule();
+            bundleManager.setProperty(Module.class.getName(), frameworkModule);
 
             // Register the {@link ModuleLoader} with the {@link ClassifyingModuleLoaderService}
+            ModuleManagerPlugin moduleManagerPlugin = bundleManager.getPlugin(ModuleManagerPlugin.class);
             ServiceController<?> controller = container.getRequiredService(ClassifyingModuleLoaderService.SERVICE_NAME);
             ClassifyingModuleLoaderService moduleLoaderService = (ClassifyingModuleLoaderService) controller.getValue();
-            Value<ModuleLoader> value = new ImmediateValue<ModuleLoader>(osgiModuleLoader);
+            Value<ModuleLoader> value = new ImmediateValue<ModuleLoader>(moduleManagerPlugin.getModuleLoader());
             osgiModuleLoaderInjector = new ClassifyingModuleLoaderInjector(Constants.JBOSGI_PREFIX, value);
             osgiModuleLoaderInjector.inject(moduleLoaderService);
         } catch (Throwable t) {
@@ -156,47 +164,46 @@ public class BundleManagerService implements Service<BundleManager> {
     static class FrameworkModuleLoader extends ModuleLoader {
 
         private final ModuleSpec moduleSpec;
-        private final ModuleIdentifier frameworkIdentifier;
-        private Module defaultFrameworkModule;
 
-        FrameworkModuleLoader(ModuleLoader moduleLoader, Map<String, Object> props) throws ModuleLoadException {
+        FrameworkModuleLoader(BundleManager bundleManager) throws ModuleLoadException {
 
-            defaultFrameworkModule = moduleLoader.loadModule(ModuleIdentifier.create("org.jboss.osgi.framework"));
-            String modulesProps = (String) props.get(Configuration.PROP_JBOSS_OSGI_SYSTEM_MODULES);
-            if (modulesProps == null) {
-                frameworkIdentifier = defaultFrameworkModule.getIdentifier();
-                moduleSpec = null;
-                return;
-            }
+            ModuleLoader moduleLoader = bundleManager.getDefaultModuleLoader();
+            Module frameworkModule = moduleLoader.loadModule(ModuleIdentifier.create("org.jboss.osgi.framework"));
 
-            // Setup the extended framework moduel spec
-            frameworkIdentifier = ModuleIdentifier.create("org.jboss.osgi.framework.extended");
+            // Setup the extended framework module spec
+            ModuleIdentifier frameworkIdentifier = ModuleIdentifier.create("org.jboss.osgi.framework.extended");
             ModuleSpec.Builder builder = ModuleSpec.build(frameworkIdentifier);
             PathFilter all = PathFilters.acceptAll();
 
             // Add a dependency on the default framework module
-            ModuleIdentifier moduleId = defaultFrameworkModule.getIdentifier();
+            ModuleIdentifier moduleId = frameworkModule.getIdentifier();
             DependencySpec moduleDep = DependencySpec.createModuleDependencySpec(all, all, moduleLoader, moduleId, false);
             builder.addDependency(moduleDep);
 
             // Add the user defined module dependencies
-            for (String moduleProp : modulesProps.split(",")) {
-                moduleId = ModuleIdentifier.create(moduleProp.trim());
-                moduleDep = DependencySpec.createModuleDependencySpec(all, all, moduleLoader, moduleId, false);
-                builder.addDependency(moduleDep);
+            String modulesProps = (String) bundleManager.getProperty(Configuration.PROP_JBOSS_OSGI_SYSTEM_MODULES);
+            if (modulesProps != null) {
+                for (String moduleProp : modulesProps.split(",")) {
+                    moduleId = ModuleIdentifier.create(moduleProp.trim());
+                    moduleDep = DependencySpec.createModuleDependencySpec(all, all, moduleLoader, moduleId, false);
+                    builder.addDependency(moduleDep);
+                }
             }
+
+            // Add a dependency on the system module
+            PathFilter exp = PathFilters.in(bundleManager.getPlugin(SystemPackagesPlugin.class).getExportedPaths());
+            moduleDep = DependencySpec.createModuleDependencySpec(all, exp, moduleLoader, ModuleIdentifier.SYSTEM, false);
+            builder.addDependency(moduleDep);
 
             moduleSpec = builder.create();
         }
 
         Module getFrameworkModule() throws ModuleLoadException {
-            return (moduleSpec != null ? loadModule(moduleSpec.getModuleIdentifier()) : defaultFrameworkModule);
+            return loadModule(moduleSpec.getModuleIdentifier());
         }
 
         @Override
         protected ModuleSpec findModule(ModuleIdentifier identifier) throws ModuleLoadException {
-            if (moduleSpec == null || identifier.equals(frameworkIdentifier) == false)
-                throw new IllegalStateException("Cannot provide ModuleSpec for: " + identifier);
             return moduleSpec;
         }
 
